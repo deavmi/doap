d6a9f7d32945f329027e5cca582547f8dbd445a2
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-09-11 22:18:19 +0200

    Packet
    - Defined `CoapPacket` to represent a CoAP packet
    - Added the required fields
    - Constructor sets the version to `1` (the only version available)
    - `getBytes()` no encodes the first byte of the CoAP header (versio, type and token length)

---
+++ source/doap/packet.d
@@ -0,0 +1,36 @@
+module doap.packet;
+
+import doap.codes : Code;
+
+/** 
+ * Represents a CoAP packet
+ */
+public class CoapPacket
+{
+    private ubyte ver, type;
+    private ubyte tokenLen;
+    private Code code;
+    private ushort mid;
+    private ubyte[] token;
+    private uint options;
+    private ubyte[] payload;
+
+    this()
+    {
+        // Set the version (Default is 1)
+        ver = 1;
+    }
+
+    public ubyte[] getBytes()
+    {
+        ubyte[] encoded;
+
+        // Calculate the first byte (ver | type | tkl)
+        ubyte firstByte = cast(ubyte)(ver << 6);
+        firstByte = firstByte | cast(ubyte)(type << 4);
+        firstByte = firstByte | tokenLen;
+        
+        return encoded;
+    }
+
+}
