faa9bc1bc1ddbdc48bd119920f8dd4ec5272f852
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-09-22 17:49:10 +0200

    CoapMessagingLayerFR
    - Added method `getEndpointAddress()`

--- source/doap/client/mesglayer.d
+++ source/doap/client/mesglayer.d
@@ -1,6 +1,7 @@
 module doap.client.mesglayer;
 
 import doap.client.client : CoapClient;
+import std.socket : Address;
 
 public abstract class CoapMessagingLayerFR
 {
@@ -30,5 +31,16 @@ public abstract class CoapMessagingLayerFR
     public final CoapClient getClient()
     {
         return this.client;
+    }
+
+    /** 
+     * Retrieves the CoAP endpoint the client is
+     * connected to
+     *
+     * Returns: the endpoint address
+     */
+    protected final Address getEndpointAddress() // Final in Interface
+    {
+        return this.client.address;
     }
 }
