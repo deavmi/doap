0494a0fd744e365de032d08aa0734e63ddccf1ad
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-09-22 17:43:13 +0200

    CoapMessagingLayerFR
    - Added missing imports
    - Added `getClient()` method
    - Removed the super constructor call

--- source/doap/client/mesglayer.d
+++ source/doap/client/mesglayer.d
@@ -1,6 +1,34 @@
 module doap.client.mesglayer;
 
+import doap.client.client : CoapClient;
+
 public abstract class CoapMessagingLayerFR
 {
-    
+    /** 
+     * The client
+     */
+    private CoapClient client;
+
+    /** 
+     * Constructs a new messaging layer instance
+     * associated with the provided client
+     *
+     * Params:
+     *   client = the client
+     */
+    this(CoapClient client)
+    {
+        this.client = client;
+    }
+
+    /** 
+     * Retrieves the client associated with
+     * this messaging layer
+     *
+     * Returns: the `CoapClient`
+     */
+    public final CoapClient getClient()
+    {
+        return this.client;
+    }
 }
