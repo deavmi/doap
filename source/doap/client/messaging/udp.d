1bc4664e3302660b1bd1b541f0f71c5bfb55c8aa
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-09-25 20:05:20 +0200

    UDPMessaging
    - `handlePacket(CoapPacket)` now calls `yankRequest()` with the received and decoded `CoapPacket`

--- source/doap/client/messaging/udp.d
+++ source/doap/client/messaging/udp.d
@@ -199,7 +199,7 @@ public class UDPMessaging : CoapMessagingLayer
      */
     private void handlePacket(CoapPacket packet)
     {
-        CoapRequest request = getClient().yankRequest(packet.getToken());
+        CoapRequest request = getClient().yankRequest(packet);
         if(request)
         {
             writeln("Matched response '"~packet.toString()~"' to request '"~request.toString()~"'");
