33648d5e44a2bf6449c32c73db85a56a71558b5e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-09-25 14:16:54 +0200

    Package (server)
    - Added module stub
    
    Package (general)
    
    - Documented
    - Added `doap.server` import

--- source/doap/package.d
+++ source/doap/package.d
@@ -1,7 +1,25 @@
+/**
+ * doap is a CoAP library for the D programming language.
+ */
 module doap;
 
+/**
+ * CoAP client implementation
+ */
 public import doap.client;
 
+/**
+ * CoAP server implementation
+ */
+public import doap.server;
+
+/**
+ * CoAP packet format serialization
+ * and deserialization
+ */
 public import doap.protocol;
 
+/**
+ * Exception definitions
+ */
 public import doap.exceptions;
